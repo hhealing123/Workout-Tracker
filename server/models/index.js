a665ac8ef2377438dab49c329503d1683bf372a4
mramazzini <mramazzini123@gmail.com>
2022-11-28 19:24:53 -0600

    Update index.js

--- server/models/index.js
+++ server/models/index.js
@@ -1,4 +1,4 @@
-const Tech = require('./Tech');
-const Matchup = require('./Matchup');
+const Workouts = require('./Workouts');
+const User = require('./User');
 
-module.exports = { Tech, Matchup };
+module.exports = { Workouts, User };
