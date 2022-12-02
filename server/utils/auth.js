06a434477a2f2681444f630fffdba20a9200bee0
Harvey Lee <hhealing123@gmail.com>
2022-12-02 16:59:15 -0600

    Adding more codes to connection.js and adding authentication middleware to auth.js

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,11 +1,38 @@
 const jwt = require('jsonwebtoken');
 
+// Sets up token secret and expiration date
 const secret = 'secretthatnooneknows';
 const expiration = '1h';
 
 module.exports = {
-  signToken: function ({ email, name, _id }) {
-    const payload = { email, name, _id };
+  // authMiddleware function will authenticate the routes
+  authMiddleware: function (req, res, next) {
+    // Allows token to be sent via req.query or headers
+    let token = req.query.token || req.headers.authorization;
+
+    if (req.headers.authorization) {
+      token = token.split(' ').pop().trim();
+    }
+    if (!token) {
+      return res.status(400).json({ message: 'There is no token... Please try again!' });
+    }
+    // Verify token and will get user data from it
+    try {
+      const { data } = jwt.verify(token, secret, { maxAge: expiration });
+      req.user = data;
+    } catch {
+      console.log('Invalid token... Please try again!');
+      return res.status(400).json({ message: "Invalid token... Please try again!" });
+    }
+    
+    // Allows to send to next end-point
+    next();
+
+  },
+
+  signToken: function ({ username, email, _id }) {
+    const payload = { username, email, _id };
     return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
   },
+
 };
