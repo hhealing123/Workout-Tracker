de11ab2cc7922597cc848b7eeb05e27ae1516c0e
mramazzini <mramazzini123@gmail.com>
2022-12-03 12:55:19 -0600

    Fixed Apolo graphql endpoint and auth

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -4,8 +4,32 @@ const secret = 'secretthatnooneknows';
 const expiration = '1h';
 
 module.exports = {
-  signToken: function ({ email, name, _id }) {
-    const payload = { email, name, _id };
+  // authMiddleware function will authenticate the routes
+  authMiddleware: function ({ req }) {
+    // allows token to be sent via req.body, req.query, or headers
+    let token = req.body.token || req.query.token || req.headers.authorization;
+
+    // ["Bearer", "<tokenvalue>"]
+    if (req.headers.authorization) {
+      token = token.split(' ').pop().trim();
+    }
+
+    if (!token) {
+      return req;
+    }
+
+    try {
+      const { data } = jwt.verify(token, secret, { maxAge: expiration });
+      req.user = data;
+    } catch {
+      console.log('Invalid token');
+    }
+
+    return req;
+  },
+
+  signToken: function ({ username, email, _id }) {
+    const payload = { username, email, _id };
     return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
   },
 };
