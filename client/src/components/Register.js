95114584a687ed30c637226197207baa379807a0
mramazzini <mramazzini123@gmail.com>
2022-12-05 20:37:59 -0600

    Register works

--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -5,9 +5,7 @@ import Auth from './utils/auth';
 import { ADD_USER } from './utils/mutations';
 
 const Register = (props) => {
-    const [email, setEmail] = useState('');
-    const [pass, setPass] = useState('');
-    const [username, setName] = useState('');
+    const [formState, setFormState] = useState({ email: '', password: '' });
     const [addUser] = useMutation(ADD_USER);
 
     const handleSubmit = async (e) => {
@@ -15,12 +13,10 @@ const Register = (props) => {
         try{
         const mutationResponse = await addUser({
             
-
-            
              variables: {
-              email: email.email,
-              password: pass.password,
-              username: username.username,
+              email: formState.email,
+              password: formState.password,
+              username: formState.username,
             },
           });
           const token = mutationResponse.data.addUser.token;
@@ -30,16 +26,24 @@ const Register = (props) => {
         }
     };
 
+    const handleChange = (event) => {
+        const { name, value } = event.target;
+        setFormState({
+          ...formState,
+          [name]: value,
+        });
+      };
+
     return (
         <div className="auth-form-container">
             <h2>Register</h2>
         <form className="register-form" onSubmit={handleSubmit}>
             <label htmlFor="name">Full Name</label>
-            <input value={username} onChange={(e) => setName(e.target.value)} name="name" id="name" placeholder="UserName"/>
+            <input onChange={handleChange} name="username" id="username" placeholder="UserName"/>
             <label htmlFor="email">Email</label>
-            <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" placeholder="example@email.com" id="email" name="email" />
+            <input onChange={handleChange} type="email" placeholder="example@email.com" id="email" name="email" />
             <label htmlFor="password">Password</label>
-            <input value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder="*********" id="password" name="password" />
+            <input  onChange={handleChange} type="password" placeholder="*********" id="password" name="password" />
             <button type="submit">Sign Up</button>
         </form>
         <Link className="link-btn" to="/login">Already have an account? Login here</Link>
